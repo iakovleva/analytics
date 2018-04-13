56a6e230bb1fc45aad71f1509610b97cb03c2dfb
iakovleva <paulina.iakovleva@gmail.com>
2018-04-13 14:46:22 +0300

    Change number of clicks instead of deleting the whole row

--- kw_report.py
+++ kw_report.py
@@ -9,23 +9,30 @@ names = ['clicks', 'convertions', 'ConvVal', 'cost', 'ConvVal_cost',
 for threshold in range(1, 16):
 
     dataset = pd.read_csv(file, names=names)
-    dropped_rows = []
+    changed_rows = []
+    # convert columns to numeric
+    clicks = pd.to_numeric(dataset['clicks'], errors='coerce')
+    ConvVal_cost = pd.to_numeric(dataset['ConvVal_cost'], errors='coerce')
+    clicks_conv = pd.to_numeric(dataset['clicks_conv'], errors='coerce')
+    ConvVal_clicks = pd.to_numeric(dataset['ConvVal_clicks'], errors='coerce')
+    cost_clicks = pd.to_numeric(dataset['cost_clicks'], errors='coerce')
 
     # check every row if its data meets the conditions
     for x in range(1, len(dataset.index)):
         row = dataset.iloc[x]
-        click = pd.to_numeric(row[0])
-        ConvVal_cost = pd.to_numeric(row[4])
-        clicks_conv = pd.to_numeric(row[6])
-        if click >= threshold and ConvVal_cost < 0.8 and clicks_conv > threshold:
-            dropped_rows.append(x)
-    dataset = dataset.drop(dataset.index[dropped_rows])
+        if clicks[x] >= threshold and ConvVal_cost[x] < 0.8 and clicks_conv[x] > threshold:
+            dataset.loc[x, 'clicks'] = threshold
+            dataset.loc[x, 'ConvVal'] = dataset.ConvVal_clicks[x] * threshold
+            dataset.loc[x, 'cost'] = dataset.cost_clicks[x] * threshold
+            dataset.loc[x, 'profit'] = (ConvVal_clicks[x] * threshold)-(cost_clicks[x] * threshold)
+            changed_rows.append(x)
+
     # count the average yield after deleting keys
-    new_yield = pd.to_numeric(dataset.ConvVal_cost[1:]).mean()
+    new_yield = ConvVal_cost[1:].mean()
     profit = pd.to_numeric(dataset.profit[1:]).sum()
-    print("threshold: %s, dropped keys %s, remained keys %s, yield %s, profit %s" % (
+    print("threshold: %s, changed keys %s, unchanged keys %s, yield %s, profit %s" % (
             threshold,
-            len(dropped_rows),
+            len(changed_rows),
             len(dataset.index),
             new_yield,
             profit,
